781a362bf0e12ed154a2765afa3e302ab96e5426
Athul-Krishnan-A-S <athulkrishnan@seemymachines.com>
2024-07-15 17:19:59 +0530

    .

--- usingPromises/employeeDashboard.js
+++ usingPromises/employeeDashboard.js
@@ -1,3 +1,4 @@
+let existingData = [];
 
 document.addEventListener('DOMContentLoaded',function(){
     fetchData()
@@ -56,7 +57,10 @@ function displayData(data){
     div.appendChild(designationField);
 
     const dob = new Date(obj.dob);
-    const DateOfBirth = dob.toLocaleDateString('en-US');
+    const day = dob.getDate().toString().padStart(2,'0');
+    const month = (dob.getMonth() + 1).toString().padStart(2,'0');
+    const year = dob.getFullYear().toString();
+    const DateOfBirth = day+"/"+month+"/"+year;
     const dateOfBirthField = document.createElement('input');
     dateOfBirthField.value = DateOfBirth;
     dateOfBirthField.setAttribute('disabled','');
@@ -88,7 +92,15 @@ function displayData(data){
 
 function addUser(){
     const modal = document.getElementById('modal');
+    const btn = document.getElementById('editAddbtn');
+    btn.innerHTML = 'ADD USER';
     modal.classList.remove('hidden');
+
+    document.getElementById('modal-nameField').value = "";
+    document.getElementById('modal-designationField').value = "";
+    document.getElementById('modal-dateField').value = "";
+    document.getElementById('modal-dateField').removeAttribute('type');
+    document.getElementById('modal-dateField').setAttribute('type','date');
 }
 
 function editUser(id){
@@ -106,6 +118,9 @@ function editUser(id){
     const designation = document.getElementById('modal-designationField');
     const dob = document.getElementById('modal-dateField');
 
+    const btn = document.getElementById('editAddbtn');
+    btn.innerHTML = 'SAVE';
+
     dob.removeAttribute('type');
     dob.setAttribute('type','text');
 
@@ -122,6 +137,9 @@ function editUser(id){
                 name.value = data.name;
                 designation.value = data.designation;
                 dob.value = new Date(data.dob).toLocaleDateString('en-US');
+
+
+                existingData.push(data);
                 resolve(data);
             })
             .catch(error => {
@@ -140,69 +158,100 @@ function updateUserDetails(id,event){
     let designation = document.getElementById('modal-designationField').value;
     let dob = document.getElementById('modal-dateField').value;
     const data = {name,designation,dob};
-    
-    return new Promise((resolve,reject) => {
-        fetch(url,{
-            method:'PUT',
-            headers:{
-                'Accept':'Application/json',
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify(data)
+    if(existingData[0].name !== name || existingData[0].designation !== designation || existingData[0].dob !== dob){
+        return new Promise((resolve,reject) => {
+            fetch(url,{
+                method:'PUT',
+                headers:{
+                    'Accept':'Application/json',
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify(data)
+            })
+            .then(response => {
+                if(!response.ok){
+                    throw new Error('connection error');
+                }else{
+                    existingData = [];
+                    return response.json();
+                }
+            })
+            .then(data => {
+                fetchData();
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            })
         })
-        .then(response => {
-            if(!response.ok){
-                throw new Error('connection error');
-            }else{
-                return response.json();
-            }
-        })
-        .then(data => {
-            fetchData();
-            resolve(data);
-        })
-        .catch(error => {
-            reject(error);
-        })
-    })
+    }else{
+        closeModal(event);
+    }
+
 }
 
 function deleteUser(id){
     const url = `https://6580190d6ae0629a3f54561f.mockapi.io/api/v1/employee/${id}`;
 
-    return new Promise((resolve,reject) => {
-        fetch(url,{
-            method:'DELETE',
-            headers:{
-                'Accept':'Application/json'
-            }
-        })
-        .then(response => {
-            if(!response.ok){
-                throw new Error('connectivity error');
-            }else{
-                return response.json();
-            }
-        })
-        .then(data => {
-            fetchData();
-            resolve(data);
-        })
-        .catch(error => {
-            reject(error);
-        })
-    }) 
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover !",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            return new Promise((resolve,reject) => {
+                fetch(url,{
+                    method:'DELETE',
+                    headers:{
+                        'Accept':'Application/json'
+                    }
+                })
+                .then(response => {
+                    if(!response.ok){
+                        throw new Error('connectivity error');
+                    }else{
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    fetchData();
+                    resolve(data);
+                })
+                .catch(error => {
+                    reject(error);
+                })
+            }) 
+        }
+      });
+
+    
 }
 
 function submitForm(event){
     event.preventDefault();
     const modal = document.getElementById('modal');
-    modal.classList.add('hidden');
-    let name = document.getElementById('modal-nameField').value;
-    let designation = document.getElementById('modal-designationField').value;
-    let dob = document.getElementById('modal-dateField').value;
+    let name = document.getElementById('modal-nameField');
+    let designation = document.getElementById('modal-designationField');
+    let dob = document.getElementById('modal-dateField');
 
-    storeData(name,designation,dob);
+    const nameRegex = /^(?! )(?!.* {2})[a-zA-Z]+$/;
+    const dobRegex = /^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])$/;
+    if(nameRegex.test(name.value) && nameRegex.test(designation.value) && dobRegex.test(dob.value)){
+        modal.classList.add('hidden');
+        storeData(name.value,designation.value,dob.value);
+    }else if(!nameRegex.test(name.value)){
+        name.focus();
+        alert("enter a valid name");
+    }else if(!nameRegex.test(designation.value)){
+        designation.focus();
+        alert("enter a valid designation");
+    }else if(!dobRegex.test(dob.value)){
+        dob.focus();
+        alert("enter a valid dob");
+    } 
 }
 
 function storeData(name,designation,dob){
